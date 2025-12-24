f1dc6069e2231226d0b1270a3b249ba7cd14da86
Vijay Gautam <vijay.gautam@shipease.in>
2025-12-25 00:07:01 +0530

    test

---
+++ crudproject/books/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
